b42e3eaacdef1aa7d75b81a4024b67e0ad979626
MHaubenstock <Michael@AirJordan.home>
2015-02-19 11:10:30 -0500

    Modified initial commit

--- FileEncrypter.java
+++ FileEncrypter.java
@@ -4,6 +4,6 @@ public class FileEncrypter
 {
 	public static void main (String args[]) throws java.io.FileNotFoundException, java.io.IOException
 	{
-		CipherBlockChain encrypter = new CipherBlockChain();
+		
 	}
 }
